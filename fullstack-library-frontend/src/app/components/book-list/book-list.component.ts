7bd5e39086f6f64cadfccb4a62babaef377c49f1
ramasai1982 <ramasami.pannirselvame@gmail.com>
2025-02-13 20:54:28 +0100

    book-list component updated with listAllBooks method

--- fullstack-library-frontend/src/app/components/book-list/book-list.component.ts
+++ fullstack-library-frontend/src/app/components/book-list/book-list.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookService } from '../../services/book.service';
+import { Book } from '../../models/book.model';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit {
+  books: Book[] = [];
 
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+    this.listAllBooks();
+  }
+
+  listAllBooks(){
+    this.bookService.getBooks().subscribe(
+      (books) => {
+      this.books = books;
+    },
+    (error: HttpErrorResponse) => {
+      alert(error.message)
+    }
+  );
+  }
 }
+
