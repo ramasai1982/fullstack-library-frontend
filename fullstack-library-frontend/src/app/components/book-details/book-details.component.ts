2c80df190160af7ee38a58a8c0763a0008aced3c
ramasai1982 <ramasami.pannirselvame@gmail.com>
2025-02-13 22:27:02 +0100

    book details component initial update

--- fullstack-library-frontend/src/app/components/book-details/book-details.component.ts
+++ fullstack-library-frontend/src/app/components/book-details/book-details.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BookService } from '../../services/book.service';
+import { Book } from '../../models/book.model';
 
 @Component({
   selector: 'app-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.css']
 })
-export class BookDetailsComponent {
 
+export class BookDetailsComponent implements OnInit {
+  book: Book | null = null;
+  errorMessage: string = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private bookService: BookService
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    if (id) {
+      this.bookService.getBookById(id).subscribe({
+        next: (data) => {
+          this.book = data;
+        },
+        error: (error) => {
+          this.errorMessage = 'Book not found or an error occurred.';
+          console.error('Error fetching book details:', error);
+        }
+      });
+    }
+  }
 }
+
