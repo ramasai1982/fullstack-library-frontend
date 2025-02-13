1940aa81ee61c34d56b424372ee6a94217b9022d
ramasai1982 <ramasami.pannirselvame@gmail.com>
2025-02-13 20:21:51 +0100

    components book-list, book-details & search-bar created

--- fullstack-library-frontend/src/app/app.module.ts
+++ fullstack-library-frontend/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    BookDetailsComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule
