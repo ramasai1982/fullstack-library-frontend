9ffcf79a32848e53128b0ed0665cfad85cf1a185
ramasai1982 <ramasami.pannirselvame@gmail.com>
2025-02-13 20:50:15 +0100

    Service book updated

--- fullstack-library-frontend/src/app/services/book.service.ts
+++ fullstack-library-frontend/src/app/services/book.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Book } from '../models/book.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
+  private apiUrl = 'http://localhost:8080/books'; // Spring Boot API
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.apiUrl);
+  }
+
+  getBookById(id: number): Observable<Book> {
+    return this.http.get<Book>(`${this.apiUrl}/${id}`);
+  }
+
+  searchBooks(title?: string, author?: string): Observable<Book[]> {
+    let searchUrl = `${this.apiUrl}/search`;
+    if (title) searchUrl += `?title=${title}`;
+    if (author) searchUrl += `&author=${author}`;
+    return this.http.get<Book[]>(searchUrl);
+  }
 }
+
